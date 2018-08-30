814a0aab612408e49d5488dbfe8800f9410902f1
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2018-08-30 11:43:21 +0200

    Update drawer.cpp

--- drawer.cpp
+++ drawer.cpp
@@ -25,21 +25,10 @@
 #define MASK_8_24_TO_32 0x000000FFFFFF0000
 #define MASK_9_24_TO_32 0xFFFFFF0000000000
 
-#define MASK_24_BITS_BLACK 0x0000000000000000
-#define MASK_24_BITS_WHITE 0x00FFFFFF00FFFFFF
-#define MASK_24_BITS_RED   0x00FF000000FF0000
-#define MASK_24_BITS_BLUE  0x000000FF000000FF
-#define MASK_24_BITS_GREEN 0x0000FF0000000000
-
 #define ALIGN_64(X)     ((X) + 63) & ~63
 #define ALIGN_32(X)     ((X) + 31) & ~31
 #define ALIGN_16(X)     ((X) + 15) & ~15
 #define ALIGN_8(X)      ((X) + 7 ) & ~7
-
-#define IS_ALIGN_64(X)     (!(ALIGN_64(X)%64))
-#define IS_ALIGN_32(X)     (!(ALIGN_32(X)%32))
-#define IS_ALIGN_16(X)     (!(ALIGN_16(X)%16))
-#define IS_ALIGN_8(X)      (!(ALIGN_8(X) %8 ))
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 typedef struct fb_var_screeninfo VariableScreen;
 typedef struct fb_fix_screeninfo FixScreen;
@@ -58,9 +47,9 @@ inline unsigned int pixel_color(unsigned char   r,
 	return (r << vinfo->red.offset) | (g << vinfo->green.offset) | (b << vinfo->blue.offset);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-static void fast_convert_24_bit_to_32_bit(const void*  _24bits,
-	                                   void*        _32bits,
-	                                   const size_t _32bits_bytes)
+static void fast_convert_24bit_to_32bit(const void*  _24bits,
+	                                void*        _32bits,
+	                                const size_t _32bits_bytes)
 {
 	unsigned long long* src = (unsigned long long*)_24bits;
 	unsigned long long* trg = (unsigned long long*)_32bits;
@@ -69,14 +58,116 @@ static void fast_convert_24_bit_to_32_bit(const void*  _24bits,
 		*trg++ = ((*src   & MASK_0_24_TO_32) << 8 ) | ((*src   & MASK_1_24_TO_32) << 16);
 		*trg++ = ((*src++ & MASK_2_24_TO_32) >> 40) | ((*src   & MASK_3_24_TO_32) << 24) | ((*src & MASK_4_24_TO_32) << 32);
 		*trg++ = ((*src   & MASK_5_24_TO_32) >> 24) | ((*src++ & MASK_6_24_TO_32) >> 16) | ((*src & MASK_7_24_TO_32) << 48);
-		*trg++ = ((*src   & MASK_8_24_TO_32) >> 8 ) |  (*src++ & MASK_9_24_TO_32);
+		*trg++ = ((*src   & MASK_8_24_TO_32) >> 8 ) | (*src++  & MASK_9_24_TO_32);
+	} while (byte -= 32);
+	src  = (unsigned long long*)_32bits;
+	byte = _32bits_bytes;
+	do {
+		*src++ = (*src >> 8);
+		*src++ = (*src >> 8);
+		*src++ = (*src >> 8);
+		*src++ = (*src >> 8);
 	} while (byte -= 32);
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-inline void display(const char*     address, 
-		     FixScreen*      pfinfo, 
-		     VariableScreen* pvinfo, 
-		     unsigned char*  screen_start)
+static void fast_color_screen(void*              addr,
+			      unsigned long long color,
+	                      const size_t       bytes)
+{
+	unsigned long long* trg = (unsigned long long*)addr;
+	size_t byte = bytes;
+	do { *trg++ = color; } while (byte -= 8);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+static void fast_blit(void*  src, 
+		      size_t src_bytes, 
+		      size_t single_line_bytes, 
+		      size_t number_of_lines, 
+		      size_t image_width_cutoff, 
+		      void*  screen_pointer)
+{
+	unsigned long long* trg = (unsigned long long*)src;
+	unsigned long long* scr = (unsigned long long*)screen_pointer;
+	size_t ln = number_of_lines;
+	size_t byte = image_width_cutoff << 2;
+	do {
+		do { 
+			*scr++ = *trg++;
+	 	} while (byte -= 8);
+		byte = image_width_cutoff << 2;
+		scr += (1280>>3);    // why 1280?
+	} while(ln--);
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+static void display(const char*      address, 
+		    FixScreen*       pfinfo, 
+		    VariableScreen*  pvinfo, 
+		    unsigned char*   screen_start)
+{
+	BMP* bmp                       = nullptr;
+	FILE* f                        = fopen(address, "rb");
+	bmp                            = (BMP*)malloc(sizeof(BMP));
+	bmp->width                     = 0;
+	bmp->height            	       = 0;
+	bmp->_24bits_pixels            = nullptr;
+	bmp->_32bits_pixels            = nullptr;
+	unsigned char info[54]         = { 0 };
+	fread(info, sizeof(unsigned char), 54, f);
+	bmp->width             = *(unsigned int*)&info[18];
+	bmp->height            = *(unsigned int*)&info[22];
+	unsigned int size24    = ((bmp->width * bmp->height * 3) + 31) & ~31;
+	bmp->_24bits_pixels    = (unsigned char*)malloc(size24);
+	unsigned int size32    = ((bmp->width * bmp->height * 4) + 31) & ~31;
+	bmp->_32bits_pixels    = (unsigned char*)malloc(size32);
+	fread(bmp->_24bits_pixels, sizeof(unsigned char), size24, f);
+	fclose(f);
+
+	fast_convert_24bit_to_32bit(bmp->_24bits_pixels, 
+				    bmp->_32bits_pixels, 
+				    size32);
+
+	fast_blit(bmp->_32bits_pixels, 
+		  size32, 
+		  pfinfo->line_length, 
+		  bmp->height, 
+		  bmp->width, 
+		  screen_start);
+
+	free(bmp->_32bits_pixels);
+	free(bmp->_24bits_pixels);
+	free(bmp);
+	return;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+int main(void) {
+	FixScreen      finfo;
+	VariableScreen vinfo;
+	int fb = open("/dev/fb0", O_RDWR);
+	if(fb < 0) {
+		printf("ERROR!\n");
+		return -1;	
+	}
+	ioctl(fb, FBIOGET_VSCREENINFO, &vinfo);
+	vinfo.grayscale      = 0;
+	vinfo.bits_per_pixel = 32;
+	ioctl(fb, FBIOPUT_VSCREENINFO, &vinfo);
+	ioctl(fb, FBIOGET_VSCREENINFO, &vinfo);
+	ioctl(fb, FBIOGET_FSCREENINFO, &finfo);
+	long screensize = vinfo.yres_virtual * finfo.line_length;
+	unsigned char* fbp = (unsigned char*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb, (off_t)0);
+
+	fast_color_screen(fbp, 0x00afcb0000afcb00, screensize);
+
+	//for(int j = 0; j < 500; j++)
+		display("SampleImage.bmp", &finfo, &vinfo, fbp);
+
+	return 0;
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+static void old_display(const char*     address, 
+		         FixScreen*      pfinfo, 
+		         VariableScreen* pvinfo, 
+		         unsigned char*  screen_start)
 {
 	BMP* bmp               = nullptr;
 	FILE* f                = fopen(address, "rb");
@@ -106,32 +197,5 @@ inline void display(const char*     address,
 	free(bmp->pixels);
 	free(bmp);
 	return;
-}
-////////////////////////////////////////////////////////////////////////////////////////////////////////
-int main(void) {
-	FixScreen      finfo;
-	VariableScreen vinfo;
-	int fb = open("/dev/fb0", O_RDWR);
-	if(fb < 0) {
-		printf("ERROR!\n");
-		return -1;	
-	}
-	ioctl(fb, FBIOGET_VSCREENINFO, &vinfo);
-	vinfo.grayscale      = 0;
-	vinfo.bits_per_pixel = 32;
-	ioctl(fb, FBIOPUT_VSCREENINFO, &vinfo);
-	ioctl(fb, FBIOGET_VSCREENINFO, &vinfo);
-	ioctl(fb, FBIOGET_FSCREENINFO, &finfo);
-	long screensize = vinfo.yres_virtual * finfo.line_length;
-	unsigned char* fbp = (unsigned char*)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fb, (off_t)0);
-	for (int x = 0; x < vinfo.xres; x++) {
-		for (int y = 0; y < vinfo.yres; y++) {
-			long location = (x + vinfo.xoffset) * (vinfo.bits_per_pixel / 8) + (y + vinfo.yoffset) * finfo.line_length;
-			*((unsigned int*)(fbp + location)) = pixel_color(0x00, 0x00, 0x00, &vinfo);
-		}
-	}
-	display("SampleImage.bmp", 
-		/* by default; don't change */ &finfo, &vinfo, fbp);
-	return 0;
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
