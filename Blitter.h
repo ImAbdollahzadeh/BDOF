fe4a2e95c99580d26b409a2f4b4c0a12af4dc5f9
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2018-08-31 14:52:53 +0200

    Update Blitter.h

--- Blitter.h
+++ Blitter.h
@@ -2,6 +2,11 @@
 #ifndef B_L_I_T_T_E_R__H__
 #define B_L_I_T_T_E_R__H__
 
-
+#include <linux/fb.h>		
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>	
+#include <iostream>	
+#include <chrono>	
 
 #endif
