fcff7d31aa3828bbbb5497b56a08d4be548441e7
Iman Abdollahzadeh <iman.ab2013@gmail.com>
2018-09-03 11:17:20 +0200

    Update movie_player.c

--- src/movie_player.c
+++ src/movie_player.c
@@ -1,6 +1,33 @@
 
 #include "Blitter.h"
 
+static void write_movie_to_file(const char*   source_file,
+	                        const char*   target_file,
+	                        unsigned char first_frame)
+{
+	FILE* f = fopen(source_file, "rb");
+	unsigned int width, height, frame_number;
+	unsigned char* data;
+	unsigned char info[54] = { 0 };
+	fread(info, sizeof(unsigned char), 54, f);
+	width  = *(unsigned int*)&info[18];
+	height = *(unsigned int*)&info[22];
+	unsigned int size = ((width * height * 3) + 31) & ~31;
+	data = (unsigned char*)malloc(size);
+	fread(data, 1, size, f);
+	fclose(f);
+	f = fopen(target_file, "a");
+	if (first_frame) {
+		const char* header = "IMAN_ABDOLLAHZADEH_MOVIE_FRAMES\n";
+		fwrite(header, 1, 32, f);
+		fwrite(&width,  1, 4, f);
+		fwrite(&height, 1, 4, f);
+		fwrite(&frame_number, 1, 4, f);
+	}
+	fwrite(data, 1, size, f);
+	fclose(f);
+}
+
 static PMOVIE display_movie(PMOVIE mov, void* screen_start) {
 	if (!mov) {
 		printf("NULL ERROR\n");
